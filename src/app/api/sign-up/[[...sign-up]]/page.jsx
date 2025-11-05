2a76907074f8411e7b13e8e5c717a8dc8d06e7f9
Yash Srivastava <ysyashmks@gmail.com>
2025-11-06 00:29:49 +0530

    remove unused sign-in and sign-up pages

--- src/app/api/sign-up/[[...sign-up]]/page.jsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function SignUpPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-background">
-      <SignUp />
-    </div>
-  );
-}

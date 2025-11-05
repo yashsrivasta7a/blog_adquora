80d1043ba84f6be19da19c42f1041b73003e3e6e
Yash Srivastava <ysyashmks@gmail.com>
2025-11-05 22:33:54 +0530

    Frontend commit

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,16 +1,21 @@
-import { configDotenv } from "dotenv";
 import mongoose from "mongoose";
-app.use(configDotenv());
 
 const MONGODB_URI = process.env.MONGODB_URI;
-if(!MONGODB_URI) throw new Error("Missing MongoDb Uri from env");
-let cached = global.mongoose ||  { connection: null, promise: null };
+if (!MONGODB_URI) throw new Error("Missing MongoDB URI from env");
 
-export async function connectDB(){
-    if(cached.connection)return cached.connection;
-    if(!cached.promise){
-       cached.promise = mongoose.connect(MONGODB_URI).then((m) => m);
+let cached = global.mongoose || { connection: null, promise: null };
+
+if (!global.mongoose) {
+  global.mongoose = cached;
+}
+
+export async function connectDB() {
+  if (cached.connection) return cached.connection;
+  
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((m) => m);
   }
+  
   cached.connection = await cached.promise;
   return cached.connection;
 }
